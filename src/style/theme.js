f31f61a97234fd44be4e1fdef8533281588f8728
Nauara Melo <nauaramelo@gmail.com>
2020-06-16 01:06:29 -0300

    Adicionando nova estrutura

---
+++ src/style/theme.js
@@ -0,0 +1,16 @@
+import { createMuiTheme } from "@material-ui/core";
+import { blue } from "@material-ui/core/colors";
+
+export default createMuiTheme({
+  palette: {
+    primary: {
+      main: '#1ed760'
+    },
+
+    secondary: {
+      main: '#616467',
+      light: '#616467'
+    }
+  }
+}); 
+
