42f232afcc1463ad8c73f598e4e5a65d9d6ecef0
Nauara Melo <nauaramelo@gmail.com>
2020-06-26 22:10:03 -0300

    Inclusão dos cases Set Logged e Set Role

--- src/reducers/login.js
+++ src/reducers/login.js
@@ -1,14 +1,24 @@
 const initialState = {
-    logged: false
+    logged: false,
+    role: null
 }
 
 const login = (state = initialState, action) => {
     switch(action.type) {
         case "SET_LOGGED": {
             return {
+                ...state,
                 logged: action.payload.logged
             }
         }
+        
+        case "SET_ROLE": {
+            return {
+                ...state,
+                role: action.payload.role
+            }
+        }
+
         default: {
             return state
         }
